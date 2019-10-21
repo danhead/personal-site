8c90878f009b7c6301e758085dd296efd5741ca5
Dan Head <me@danielhead.com>
2019-10-21 21:01:51 +0100

    Add box-shadow to label

--- src/components/label.js
+++ src/components/label.js
@@ -1,20 +1,22 @@
 import React from "react"
 import styled from "styled-components"
 
-import { blue, white } from "../utils/colors"
+import { black, blue, white } from "../utils/colors"
 
 const Container = styled.div`
   position: fixed;
-  top: 8px;
-  right: -16px;
-  width: 66px;
+  top: 4px;
+  right: -20px;
+  width: 74px;
+  padding: 0.1rem;
   font-size: 0.6rem;
   font-weight: 700;
   text-align: center;
   user-select: none;
   background-color: ${blue};
+  border: 2px solid ${black};
   color: ${white};
-  padding: 0.1rem;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.6);
   transform: rotate(45deg);
 `
 
